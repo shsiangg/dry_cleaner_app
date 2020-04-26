1870dc6ba5ddafa647df4d4a75528c74e33efbb9
Stephen Hsiang <shsiangg@gmail.com>
2020-04-26 16:20:50 -0700

    refactored dashboard_item, added timers

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -5,41 +5,25 @@ import SubmittedItem from './submitted_item'
 import CompletedItem from './completed_item';
 
 export default function Dashboard(props) {
-  const [items, setItems] = useState([{state: 'empty', id: 0, name: '', number: ''}])
+  const [items, setItems] = useState([{id: 0}])
   const [itemCount, setItemCount] = useState(1)
 
-  const handleSubmit = (e, child_key) => {
-    e.preventDefault();
-    let name = e.target.name.value
-    let number = e.target.number.value
-    let cards = [...items]
-    cards[child_key] = {state: 'submit', id: child_key, name: name, number: number}
-    setItems(cards)
-  }
 
-  const handleComplete = (e, name, number, time, child_key) => {
-    e.preventDefault();
-    let cards = [...items]
-    cards[child_key] = {state: 'complete', id: child_key, name: name, number: number, time: time}
-    setItems(cards)  }
-  
   const addItem = () => {
     setItemCount(itemCount + 1)
     setItems([...items, {state: 'empty', id: itemCount}])
   }
+
+
+
   return (
     <div className="dashboard">
       {items.map((item) => {
-        if (item.state === 'empty') {
-          return <DashboardItem handleSubmit={handleSubmit} key={item.id} uid={item.id}/>
-        } else if (item.state === 'submit') {
-          return <SubmittedItem handleComplete={handleComplete} key={item.id} uid={item.id} 
-                    name={item.name} number={item.number}/>
-        } else {
-          return <CompletedItem key={item.id} name={item.name} number={item.number} time={item.time}/>
-        }
-      })} 
+        return <DashboardItem key={item.id} uid={item.id} />
+      })}
+        
       <div className="dashboard-item add-order" type="button" onClick={() => addItem()}>
+      Add Order
       </div>
     </div>
   )
--- src/components/dashboard_item.js
+++ src/components/dashboard_item.js
@@ -1,39 +1,136 @@
-import React , { useState, useEffect } from 'react';
+import React , { useState, useEffect, useRef } from 'react';
 import SubmittedItem from './submitted_item'
 import './dashboard.css'
 
 export default function DashboardItem(props) {
   const [input, setInput] = useState({})
-  
+  const [itemState, setItemState] = useState('empty')
+  const [name, setName] = useState()
+  const [number, setNumber] = useState()
+  const [startTime, setStartTime] = useState()
+  const [endTime, setEndTime] = useState()
+  const [timer, setTimer] = useState(new Date(1000 * 0).toISOString().substr(11, 8))
+  const [seconds, setSeconds] = useState(0)
+
   const handleInputChange = (e) => setInput({
     ...input,
     [e.currentTarget.name]: e.currentTarget.value
   })
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setName(e.target.name.value)
+    setNumber(e.target.number.value)
+    setItemState('submit')
+    setStartTime(new Date().toLocaleString())
+    
+  }
+
+  const handleComplete = (e, elapsed_time) => {
+    e.preventDefault()
+    setItemState('complete')
+    setEndTime(new Date().toLocaleString())
+  }
+
+  const handleCancel = (e) => {
+    e.preventDefault()
+    setItemState('canceled')
+  }
+  
+  useInterval(() => {
+    setSeconds(seconds + 1);
+    setTimer( new Date(1000 * seconds).toISOString().substr(11, 8))
+  }, itemState === 'complete' ? null : (itemState === 'submit' ? 1000 : null) );
+
+
+
+  let content = () => {
+    switch (itemState) {
+      case 'empty':
+        return (
+          <>
+        <h2>{props.uid}</h2>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input required  name="name" placeholder="Customer Name" onChange={handleInputChange} ></input>
+          <input required pattern="\d{10}"  name="number" placeholder="Phone Number" onChange={handleInputChange} ></input>
+          <input type="submit" value="Submit" ></input>
+        </form>
+        <button name="cancel" onClick={(e) => handleCancel(e, props.uid)}>Cancel</button>      
+      </>
+        )
+      case 'submit':
+       return ( 
+       <div>
+          Processing
+          <p>{name} {number}</p>
+          <p>Started at: {startTime}</p>
+          <h3>Elapsed time: {timer}</h3>
+          <h2>{props.uid}</h2>
+          <button onClick={(e) => {handleComplete(e)}}>Complete Order</button>      
+      </div>
+       )
+       case 'canceled':
+         return (<div>
+           canceled
+         </div>)
+      default:
+        return (
+        <>
+          <h2>completed</h2>
+          <p>{name} {number}</p>
+          <p>Start time: {startTime}</p>
+          <p>End time: {endTime}</p>
+          <p>Time Elapsed: {seconds}</p>
+          <h2>{props.uid}</h2>
+        </>
+        )
+    }
+
+  }
+
 
   return (
     <div className={`dashboard-item ${props.uid}`} >
-      <form onSubmit={(e) => props.handleSubmit(e, props.uid)}>
-        <input required  name="name" placeholder="Customer Name" onChange={handleInputChange} ></input>
-        <input required pattern="\d{10}"  name="number" placeholder="Phone Number" onChange={handleInputChange} ></input>
-        <input type="submit" value="Submit" ></input>
-      </form>
+     {content()}
     </div>
 
   )
 
 }
 
-/*
-  dashboard item first asks for name an dphone Number
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
 
-  after fields are submitted, item will change look of input, create "submitted at" field, and a 
-start processing button
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
 
-  when the start processing button is pressed, a "In progress..." status field will appear and a 
-"time elapsed" will appear to count the time since button pressed, and a 
-complete order button will appear
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+/* dashboard item = [{id: 0 }, {id:1}, ...]
+  responsibilities of dashboard: create and delete items
 
-  when the complete order button is pressed, the order will say completed and how long it took 
-to complete
+  responsibilities of dashboard item: handle state of item
+
+dashboard item state: input -> processing -> completed
+
+
+ <h2>{props.uid}</h2>
+      <form onSubmit={(e) => props.handleSubmit(e, props.uid)}>
+        <input required  name="name" placeholder="Customer Name" onChange={handleInputChange} ></input>
+        <input required pattern="\d{10}"  name="number" placeholder="Phone Number" onChange={handleInputChange} ></input>
+        <input type="submit" value="Submit" ></input>
+      </form>
+      <button name="cancel" onClick={(e) => props.handleCancel(e, props.uid)}>Cancel</button>
+
 */
